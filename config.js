8fae062e47387648f785e4ee1b8a65d88ef55d88
diggzhang <diggzhang@gmail.com>
2015-04-09 15:07:54 +0800

    (init) configure file init

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = { 
+    corpid: 'wx3412f502e6a35c42',
+    corpsecret: 'Yc4PJr25b3BYSE6kWNUYgYBW7l-ICBcNs9weUSdZPz6d-UMmRLo6cYFZRVpRXWl1',
+    token: 'hackthon',
+    encodingASEKey: ''
+};
+
+//{"access_token":"j_WYTjUn3jjt616-GXScumTanh0RSYxswbkAV8QL-rtT76qquw1jFO5JpBHkJghl","expires_in":7200}
